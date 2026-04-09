44201d64d10eb91fe9d5c65438d5a2e7751800c9
HLR1981 <al22760839@ite.edu.mx>
2026-04-08 19:29:22 -0700

    Update app_ensenada.py

--- Endemica_Ens_Fl/app_ensenada.py
+++ Endemica_Ens_Fl/app_ensenada.py
@@ -98,6 +98,7 @@ if archivo:
     # Preprocesamiento
     img_resized = img.resize((224, 224))
     img_array = tf.keras.utils.img_to_array(img_resized)
+    img_array = img_array / 255.0
     img_array = tf.expand_dims(img_array, 0)
     
     # Predicción
